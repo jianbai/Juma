7a8ff77b371619d9bcdf092e195a5724787ac540
jianbai <scott_wang@brown.edu>
2014-10-18 14:49:07 -0700

    implemented jquery smoothscroll

---
+++ js/ui.js
@@ -0,0 +1,34 @@
+$(function() {
+// Smooth scrolling
+
+	$("#splash-scroller").click(function() {
+		$('html, body').animate({
+    		scrollTop: $("#home").offset().top
+		}, 1000);
+	});
+
+	$("#home-scroller").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#section1").offset().top
+		}, 1000);
+	});
+
+	$("#section1-scroller").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#section2").offset().top
+		}, 1000);
+	});
+
+	$("#section2-scroller").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#section3").offset().top
+		}, 1000);
+	})
+
+// Catering form
+
+	$(".action-button").click(function() {
+		$(this).toggleClass("expand-form", 1000);
+	});
+
+});
