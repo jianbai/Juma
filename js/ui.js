085cdde4377c027952cad1db6e3cace14f2e70c0
jianbai <scott_wang@brown.edu>
2014-10-19 00:57:09 -0700

    added form placeholder

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+
+// State
+
+	var state1 = true;
+	var state2 = true;
+
+// Hide catering forms
+
+	$("#catering1").hide();
+	$("#catering2").hide();
+
 // Smooth scrolling
 
 	$("#splash-scroller").click(function() {
@@ -29,20 +40,31 @@ $(document).ready(function() {
 		$('html, body').animate({
 			scrollTop: $("#form1").offset().top
 		}, 1000);
+
+		if (state1) {
+			$("#catering1").show("blind", 400);
+		} else {
+			$("#catering1").hide("blind", 400);
+		}
+
+		state1 = !state1;
 	});
 
 	$("#button2").click(function() {
 		$('html, body').animate({
 			scrollTop: $("#form2").offset().top
 		}, 1000);
+
+		if (state2) {
+			$("#catering2").show("blind", 400);
+		} else {
+			$("#catering2").hide("blind", 400);
+		}
+
+		state2 = !state2;
 	});
 
 // Catering form
-	// $(".action-button").mouseover(function() {
-	// 	height = $("#home").css("height");
-	// 	width = $("#home").css("width");
-	// 	$("#home").toggleClass("full-screen", false);
-	// });
 
 	$(".action-button").click(function() {
 		$(this).toggleClass("expand-form", 1000);
