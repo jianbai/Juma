af9376e1bd18e81930b387c89bcb772543681e6e
jianbai <scott_wang@brown.edu>
2014-10-18 18:07:22 -0700

    added expand-form

--- js/ui.js
+++ js/ui.js
@@ -23,12 +23,19 @@ $(function() {
 		$('html, body').animate({
 			scrollTop: $("#section3").offset().top
 		}, 1000);
-	})
+	});
+
+	$("#button1").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#form1").offset().top
+		}, 1000);
+	});
 
 // Catering form
 
 	$(".action-button").click(function() {
 		$(this).toggleClass("expand-form", 1000);
+		$("#home").toggleClass("full-screen", false, 1000);
 	});
 
 });
